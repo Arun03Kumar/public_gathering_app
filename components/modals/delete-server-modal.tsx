7d43261d0d998923b339eb5866b9ec4ebce9e096
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2024-11-30 04:57:38 +0530

    delete server

--- components/modals/delete-server-modal.tsx
+++ components/modals/delete-server-modal.tsx
@@ -7,11 +7,11 @@ import { useState } from "react"
 import axios from "axios"
 import { useRouter } from "next/navigation"
 
-export const LeaveServerModal = () => {
+export const DeleteServerModal = () => {
     const {onOpen, isOpen, onClose, type, data} = useModal()
     const router = useRouter()
 
-    const isModalOpen = (isOpen && type === "leaveServer")
+    const isModalOpen = (isOpen && type === "deleteServer")
     const {server} = data
 
     const [isLoading, setIsLoading] = useState(false)
@@ -19,7 +19,7 @@ export const LeaveServerModal = () => {
     const onClick = async () => {
         try {
             setIsLoading(true)
-            await axios.patch(`/api/servers/${server?.id}/leave`)
+            await axios.delete(`/api/servers/${server?.id}`)
             onClose()
             router.refresh()
             router.push("/")
@@ -37,10 +37,11 @@ export const LeaveServerModal = () => {
             <DialogContent className="bg-white text-black p-0 overflow-hidden">
                 <DialogHeader className="pt-8 px-6">
                     <DialogTitle className="text-2xl text-center font-bold">
-                        Leave Server
+                        Delete Server
                     </DialogTitle>
                     <DialogDescription className="text-center text-zinc-500">
-                        Are you sure you want to leave <span className="font-semibold text-indigo-500">{server?.name}</span>?
+                        Are you sure you want to do this? <br />
+                        <span className="text-indigo-500 font-semibold">{server?.name}</span> will be permanently deleted.
                     </DialogDescription>
                 </DialogHeader>
                 <DialogFooter className="bg-gray-100 px-6 py-4">
