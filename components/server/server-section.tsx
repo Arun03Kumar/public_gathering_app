163d8a674962dc9e39018e9e68de5340650015f8
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2024-11-30 19:13:09 +0530

    overlay fix in edit and delete

--- components/server/server-section.tsx
+++ components/server/server-section.tsx
@@ -29,7 +29,7 @@ export const ServerSection = ({label, role, sectionType, channelType, server}: S
             </ActionTooltip>
         )}
         {role === MemberRole.ADMIN && sectionType === "members" && (
-            <ActionTooltip label="Create Channel" side="top">
+            <ActionTooltip label="Manage Members" side="top">
                 <button onClick={() => onOpen("members", {server})} className="text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition">
                     <Settings className="h-4 w-4" />
                 </button>
