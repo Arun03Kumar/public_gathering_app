8fb98bafc331cdc77dde8c3705202caba28c2b6e
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2024-12-01 18:33:06 +0530

    message api

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -4,8 +4,11 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import * as z from "zod"
 import { Form, FormControl, FormField, FormItem } from "../ui/form"
-import { Plus } from "lucide-react"
+import { Plus, Smile } from "lucide-react"
 import { Input } from "../ui/input"
+
+import axios from "axios"
+import qs from "query-string"
 
 interface ChatInputProps {
     apiUrl: string
@@ -28,8 +31,18 @@ export const ChatInput = ({apiUrl, query, name, type}: ChatInputProps) => {
 
     const isLoading = form.formState.isSubmitting
 
-    const onSubmit = async (value: z.infer<typeof formSchema>) => {
-        console.log(value)
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        try {
+            const url = qs.stringifyUrl({
+                url: apiUrl,
+                query
+            })
+
+            await axios.post(url, values)
+        }
+        catch(err) {
+            console.log(err)
+        }
     }
 
   return (
@@ -42,7 +55,10 @@ export const ChatInput = ({apiUrl, query, name, type}: ChatInputProps) => {
                             <button type="button" onClick={() => {}} className="absolute top-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center">
                                 <Plus className="text-white dark:text-[#313338]" />
                             </button>
-                            <Input disabled={isLoading} className="px-14 py-6 bg-zinc-200/90 dark:bg-zinc-700/75 border-none border-0 focus-visible:ring-0 focus-visible:ring-offset-0 text-zinc-600 dark:text-zinc-200" />
+                            <Input {...field} disabled={isLoading} placeholder={`Message ${type === "conversation" ? name : "#" + name}`} className="px-14 py-6 bg-zinc-200/90 dark:bg-zinc-700/75 border-none border-0 focus-visible:ring-0 focus-visible:ring-offset-0 text-zinc-600 dark:text-zinc-200" />
+                            <div className="absolute top-7 right-8">
+                                <Smile />
+                            </div>
                         </div>
                     </FormControl>
                 </FormItem>
